21ca93ce0b48217af8d057a73c4a321e3d3e498d
OdaepO <odaepo@gmail.com>
2023-03-08 15:17:55 +0100

    sistemato il modello

--- pertopy/perto/admin.py
+++ pertopy/perto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task, Status, Slot
+
+admin.site.register(Task)
+admin.site.register(Status)
+admin.site.register(Slot)
